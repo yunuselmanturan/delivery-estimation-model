dd4acf45cb69c15ab72cd8b6e07d45e8c89217dd
yunus turan <yunuselmanturan@gmail.com>
2026-05-09 18:13:34 +0200

    unnecesary row removal

--- delivery_estimation.py
+++ delivery_estimation.py
@@ -487,7 +487,6 @@ def evaluate_model(model, X_test, y_test, est_test):
 # --------------------------------------------------------------------------
 # 7. Plotting
 # --------------------------------------------------------------------------
-import os
 os.makedirs("results", exist_ok=True)
 
 def plot_history(history):
